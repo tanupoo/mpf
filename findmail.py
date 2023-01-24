151b34f3593c9b46fe4ec4cc7d73d733c608a43e
tanupoo <github-shoichi@tanu.org>
2023-01-25 00:26:50 +0900

    enabled to parse the string of N month.

--- findmail.py
+++ findmail.py
@@ -62,13 +62,33 @@ def find_mail(path, recursive=False, newer_than=None):
         print("  -", info.get("Subject"))
         print("  -", info.get("Path"))
 
+def datetime_before_month(dt, m):
+    delta_y = m // 12
+    delta_m = m % 12
+    y = dt.year - delta_y
+    m = dt.month - delta_m
+    if dt.month <= delta_m:
+        y -= 1
+        m += 12
+    try:
+        return datetime(y, m, dt.day, dt.hour, dt.minute, dt.second)
+    except ValueError as e:
+        if "day is out of range for month" in str(e):
+            next_month = datetime(y, m, 28) + timedelta(days=4)
+            d = (next_month - timedelta(days=next_month.day)).day
+            return datetime(y, m, d, dt.hour, dt.minute, dt.second)
+        else:
+            raise
+
 # main
 ap = argparse.ArgumentParser()
 ap.add_argument("mail_dir", help="a directory name")
 ap.add_argument("-r", action="store_true", dest="recursively",
                 help="enable to find a mail file recursively.")
 ap.add_argument("-t", action="store", dest="time_span",
-                help="specify the span string to be picked.")
+                help="specify the span string to be picked. "
+                    "The string is either xm, xd, xw, xH, or xM (x is an integer.)"
+                )
 ap.add_argument("--tz", action="store", dest="tz",
                 help="specify the timezone.")
 ap.add_argument("-d", action="store_true", dest="debug",
@@ -82,19 +102,22 @@ if opt.tz:
     dt = datetime.now(tz=tz.gettz("Asia/Tokyo"))
     ts_limit = dt.timestamp()
 if opt.time_span:
-    if r := re.match("(\d+)w", opt.time_span):
-        delta = int(r.group(1)) * 7*24*60*60
-    elif r := re.match("(\d+)d", opt.time_span):
-        delta = int(r.group(1)) * 24*60*60
-    elif r := re.match("(\d+)h", opt.time_span):
-        delta = int(r.group(1)) * 60*60
-    elif r := re.match("(\d+)m", opt.time_span):
-        delta = int(r.group(1)) * 60
+    if r := re.match("(\d+)m", opt.time_span):
+        ts_limit = datetime_before_month(dt, int(r.group(1))).timestamp()
     else:
-        raise ValueError(f"unknown format {opt.time_span}")
+        if r := re.match("(\d+)w", opt.time_span):
+            delta = int(r.group(1)) * 7*24*60*60
+        elif r := re.match("(\d+)d", opt.time_span):
+            delta = int(r.group(1)) * 24*60*60
+        elif r := re.match("(\d+)h", opt.time_span):
+            delta = int(r.group(1)) * 60*60
+        elif r := re.match("(\d+)m", opt.time_span):
+            delta = int(r.group(1)) * 60
+        else:
+            raise ValueError(f"unknown format {opt.time_span}")
+        ts_limit = (dt - timedelta(seconds=delta)).timestamp()
 else:
-    delta = 60*60 # 1 hour
-ts_limit = (dt - timedelta(seconds=delta)).timestamp()
+    ts_limit = (dt - timedelta(seconds=60*60)).timestamp()
 
 # body
 find_mail(opt.mail_dir, recursive=opt.recursively, newer_than=ts_limit)
