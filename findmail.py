887b1b179ffd415cd08501471e4d79d5424c4567
tanupoo <github-shoichi@tanu.org>
2023-01-25 16:49:04 +0900

    removed get_info and passed it to decode_mime.py

--- findmail.py
+++ findmail.py
@@ -4,25 +4,10 @@ import os
 from stat import S_ISDIR
 import time
 import argparse
-from decode_mime import decode_mime_file, decode_subject
+from decode_mime import get_mail_info
 import re
 from datetime import datetime, timedelta
-from dateutil.parser import parse as dt_parse
 from dateutil import tz
-from email.parser import Parser as emailParser
-
-def get_info(
-        path: str,
-        ) -> dict:
-    filename, ext = os.path.splitext(path)
-    info = {}
-    info.update({"Path": path})
-    with open(path) as fd:
-        items = dict(emailParser().parse(fd))
-    info.update({"Date": dt_parse(items.get("Date"))})
-    info.update({"Subject": decode_subject(items.get("Subject"))})
-    info.update({"From": decode_subject(items.get("From"))})
-    return info
 
 def find_mail(path, recursive=False, newer_than=None):
     mails = []  # updated in walk_dir()
@@ -39,7 +24,7 @@ def find_mail(path, recursive=False, newer_than=None):
                 elif newer_than:
                     if entry.stat().st_mtime >= newer_than:
                         # this is the 1st filter.
-                        # The 2nd filter will be done later at get_info().
+                        # The 2nd filter will be done later at get_mail_info().
                         mails.append(entry)
                     else:
                         pass
@@ -56,7 +41,9 @@ def find_mail(path, recursive=False, newer_than=None):
         return
     walk_dir(path, recursive, newer_than)
     for e in sorted(mails, key=lambda x: x.stat().st_mtime):
-        info = get_info(e.path)
+        if opt.debug:
+            print("-->", e.path)
+        info = get_mail_info(e.path)
         print("##", info.get("Date"))
         print("  -", info.get("From"))
         print("  -", info.get("Subject"))
